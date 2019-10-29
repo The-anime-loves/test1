99b0c7f413fb383a58442a09761a271ca28d9c65
noahkuhne12 <50324963+noahkuhne12@users.noreply.github.com>
2019-10-29 12:47:38 +0100

    HELP MEMEMEEMMEMEMMEME
    hej med dig
    :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana:

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
-//hej med dig
-//hej med dig
-//hej med dig
-//hej med dig
-//hej med dig
-//hej med dig
-//hej med dig
-//hej med dig
-//hej med fjasjfjkeahsjkfhkhkhig
-//hej med dig
-//hej med dig
-//hej med dig
-//aefasfargar
+<?php
+    
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>index</title>
+        <link rel="stylesheet" type="text/css" href="css\index.css">  
+    </head>
+    <body>
+    <h1>hej med dig</h1>
+    </body>
+</html>
 
